21851755000ee445282ddd8b57733d3639a120cb
JoannaTanveer <joanna.tanveer@gmail.com>
2020-11-16 21:50:06 -0800

    as

--- config/connection.js
+++ config/connection.js
@@ -17,7 +17,19 @@ connection.connect(function(err) {
     return;
   }
   console.log("connected as id " + connection.threadId)
-  connection.query(schema)
+  connection.query(`DROP DATABASE IF EXISTS hamburger_db;
+  CREATE DATABASE hamburger_db;
+  
+  USE hamburger_db;
+  DROP TABLE IF EXISTS types;
+  
+  CREATE TABLE types (
+      id INT AUTO_INCREMENT NOT NULL,
+      name VARCHAR(255) NOT NULL,
+      eaten BOOLEAN DEFAULT false,
+      PRIMARY KEY (id)
+  );
+  `)
   });
 
 
